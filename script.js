a5e672d2b4db9ca2f88beca64b5cab59d1695fbd
Buelent <ekibuli@hotmail.dem>
2022-08-08 14:59:06 +0200

    add update

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 let fields = [];
 let gameOver = false;
 let currentShape ='cross'
-// von kreuz zu kreis ändern
+// von kreuz zu kreis ändern (aktiver Player)
 function fillShape(id) {
     //bug weg machen. Erst Aufrufen, wenn keine Form eingefügt ist.
     if (!fields[id] && !gameOver) {
         if(currentShape == 'cross') {
             currentShape = 'circle';
-            document.getElementById('player-1').classList.remove('player-inactiv');
-            document.getElementById('player-2').classList.add('player-inactiv');
+            document.getElementById('player-2').classList.remove('player-inactiv');
+            document.getElementById('player-1').classList.add('player-inactiv');
         
     }   else {
             currentShape = 'cross';
-            document.getElementById('player-1').classList.add('player-inactiv');
-            document.getElementById('player-2').classList.remove('player-inactiv');
+            document.getElementById('player-2').classList.add('player-inactiv');
+            document.getElementById('player-1').classList.remove('player-inactiv');
     }
 
     fields [id] = currentShape;
@@ -28,9 +28,6 @@ function draw() {
         if(fields[i] == 'circle') {
             document.getElementById('circle-' + i).classList.remove('d-none');
         }
-    }
-
-    for(let i = 0; i < fields.length; i++){
         if(fields[i] == 'cross') {
             document.getElementById('cross-' + i).classList.remove('d-none');
         }
@@ -42,22 +39,17 @@ function restart() {
     document.getElementById('itsOver').classList.add('d-none');
     document.getElementById('restartButton').classList.add('d-none');
 
-    document.getElementById('line-1').classList.add('d-none');
-    document.getElementById('line-2').classList.add('d-none');
-    document.getElementById('line-3').classList.add('d-none');
-    document.getElementById('line-4').classList.add('d-none');
-    document.getElementById('line-5').classList.add('d-none');
-    document.getElementById('line-6').classList.add('d-none');
-    document.getElementById('line-7').classList.add('d-none');
-    document.getElementById('line-8').classList.add('d-none');
     
-    // oder so  for(let i=1; i < 8; i++) {
-    //          document.getElementById('line-' + i).classList.add('d-none');}
+    for(let i = 1; i < 9; i++) {
+        document.getElementById('line-' + i).style.transform = 'scaleX(0)';
+//      document.getElementById('line-' + i).classList.add('d-none');
+    }
 
     for(let i = 0; i < 9; i++){
         document.getElementById('circle-' + i).classList.add('d-none');
         document.getElementById('cross-' + i).classList.add('d-none');
     }
+    document.getElementById('player-1').classList.remove('player-inactiv');
 }
 
 function checkForWin() {
@@ -80,39 +72,85 @@ function checkForWin() {
     //Reihe links vertikal
     if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]){
         winner= fields[0];
-        document.getElementById('line-4').style.transform = '';
+        document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(1)';
     }
     //Reihe mitte vertikal
     if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]){
         winner= fields[1];
-        document.getElementById('line-5').style.transform = '';
+        document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(1)';
     }
     //Reihe rechts vertikal
     if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]){
         winner= fields[2];
-        document.getElementById('line-6').style.transform = '';
+        document.getElementById('line-6').style.transform = 'rotate(90deg) scaleX(1)';
     }
     //Reihe diagonal links oben rechts unten
     if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]){
         winner= fields[0];
-        document.getElementById('line-8').style.transform = 'scaleX(1) scaleY(1)';
+        document.getElementById('line-8').style.transform = 'rotate(-135deg) scaleX(1) scaleY(1)';
     }
     //Reihe diagonal rechts oben links unten
     if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]){
         winner= fields[2];
-        document.getElementById('line-7').style.transform = 'scaleX(1) scaleY(1)';
+        document.getElementById('line-7').style.transform = 'rotate(135deg) scaleX(1) scaleY(1)';
     }
+    showWinner(winner);
 
-    if (winner) {
-        console.log('GEWONNEN', winner);
+//    if (winner) {
+//        console.log('GEWONNEN', winner);
+//        gameOver = true;
+//        setTimeout(function(){
+//            document.getElementById('itsOver').classList.remove('d-none');
+//        }, 1000 );
+//
+//        setTimeout(function(){
+//            document.getElementById('restartButton').classList.remove('d-none');
+//        }, 2000 );
+
+//    }
+}
+
+    function showWinner(winner) {
+        if (winner) {
+            if (winner == 'circle') {
+            gameOver = true;
+            showCircleAsWinner();
+        }   else { if (winner == 'cross') {
+            gameOver = true;
+            showCrossAsWinner();
+        } }
+    }   else { if(checkIfGameIsDraw()){
         gameOver = true;
-        setTimeout(function(){
-            document.getElementById('itsOver').classList.remove('d-none');
-        }, 1000 );
-
-        setTimeout(function(){
-            document.getElementById('restartButton').classList.remove('d-none');
-        }, 2000 );
-
+        drawResult();
     }
+    }
+}
+    function checkIfGameIsDraw() {
+        return GameDraw = (fields.filter(String).length == 9) ? true : false;
+}
+
+    function showCrossAsWinner() {
+        setTimeout(function () {
+            document.getElementById('itsOver').classList.remove('d-none');
+            document.getElementById('game-over-window').innerHTML = `
+                <div class="winner-statement"> <img class="img-winner" src="img/cross.png" alt=""> hat gewonnen!</div>
+                <button id="restartButton" class="restart-btn" onclick="restart()">Restart</button>`
+            }, 1000);
+}
+
+    function showCircleAsWinner() {
+        setTimeout(function () {
+            document.getElementById('itsOver').classList.remove('d-none');
+            document.getElementById('game-over-window').innerHTML = `
+                <div class="winner-statement"> <img class="img-winner" src="img/circle.png" alt=""> hat gewonnen!</div>
+                <button id="restartButton" class="restart-btn" onclick="restart()">Restart</button>`
+            }, 1000);
+}
+    function drawResult() {
+        setTimeout(function () {
+            document.getElementById('itsOver').classList.remove('d-none');
+            document.getElementById('game-over-window').innerHTML = `  
+            <div id="draw" class="winner-statement">Draw!</div>
+            <button id="restartButton" class="restart-btn" onclick="restart()">Restart</button>`;
+            }, 1000);
 }
